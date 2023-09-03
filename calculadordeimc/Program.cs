177cdb188d24431592cd989dfd5759276c382cd5
virtu <eltororo322@gmail.com>
2023-09-03 16:21:49 -0300

    Adicionar arquivos de projeto.

---
+++ calculadordeimc/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("calculador de indicie de imc");
+
+Console.WriteLine("Digite seu peso");
+    
+    double p = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite sua altura");
+   
+   double a = Int32.Parse(Console.ReadLine());
+
+
+double imc = (a * a) / p;
+
+
+if (imc == 18)
+{
+    Console.WriteLine("seu peso esta otimo");
+}
+
+
+else if (imc < 11)
+{
+    Console.WriteLine("seu peso esta baixo");
+}
+
+else if (imc < 24)
+{
+    Console.WriteLine("esta acima do peso");
+}
+Console.WriteLine(imc);
